9869064f713bcfd4755c95dfa0ca0195bc9d456d
JasonLMoffit <j.moffit@hotmail.com>
2020-07-12 12:03:51 -0500

    retry

---
+++ builder.js
@@ -0,0 +1,16 @@
+const baruch = require("baruch-2.json");
+
+console.log(baruch);
+const book = [
+  {
+    book: {
+      chapters: {
+        chapter: {
+          "1": {
+            verses: {},
+          },
+        },
+      },
+    },
+  },
+];
